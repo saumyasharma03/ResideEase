64b2cf1be1a169a484619adf47aa284a906ad75f
saumyasharma03 <saumya.sharma0915@gmail.com>
2025-04-02 23:57:32 +0530

    other fixes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,13 +3,10 @@ import Login from "./pages/Login";
 import Signup from "./pages/SignUp";
 import Home from "./pages/Home";
 import ExploreHotels from "./pages/ExploreHotels";
-<<<<<<< HEAD
-import HotelDetailPage from "./pages/HotelDetails";
-=======
 import Admin from "./components/Admin";
 import Fetch from "./components/Fetch";
 import {FetchProvider} from "./context/FetchContext";
->>>>>>> 7430426 (admin panel created)
+import HotelDetailPage from "./pages/HotelDetails"
 // import { AuthProvider } from "./context/AuthContext";
 
 
@@ -19,19 +16,15 @@ function App() {
     <FetchProvider>
      
         <Routes>
-          {/* <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/explore-hotels" element={<ExploreHotels />} />
-<<<<<<< HEAD
           <Route path="/explore-pgs" element={<ExploreHotels />} />
           <Route path="/hotels/:hotelId"  element={<HotelDetailPage/>}/>
-=======
-          <Route path="/explore-pgs" element={<ExploreHotels />} /> */}
           <Route path="/admin/fetchItems" element={<Fetch />} />
           <Route path="/admin" element={<Admin />} />
 
->>>>>>> 7430426 (admin panel created)
         </Routes>
      </FetchProvider>
       </>
